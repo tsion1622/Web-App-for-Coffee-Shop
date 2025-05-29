235aaf759964f460e8640295954ea3ea0522a76c
Tsion Tamirat <tayu.16.enye@gmail.com>
2025-05-29 12:22:01 +0300

    add routing for coffee views and templates including cart and payments

--- coffee/urls.py
+++ coffee/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+import coffee.views as views
+
 
 urlpatterns = [
-    path('',views.home)
+    path('', views.home, name='home'),
+    path('coffee/', views.coffee_list, name='coffee_list'),
+    #path('cart/', views.cart_view, name='cart_view'),
+    path('login/', views.login_view, name='login'),
+    path('initiate-payment/', views.initiate_payment_view, name='initiate_payment'),
+    path('payment-success/', views.payment_success, name='payment_success'),
+    path('payment-failed/', views.payment_failed, name='payment_failed'),
+    path('verify-payment/', views.verify_payment, name='verify_payment'),
+    
+    
+
 
 ]
