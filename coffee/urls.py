e923ed853600218c097ca3f8563cf9074ef1f085
Tsion Tamirat <tayu.16.enye@gmail.com>
2025-05-30 20:42:20 +0300

    new path added

--- coffee/urls.py
+++ coffee/urls.py
@@ -1,20 +1,14 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
-import coffee.views as views
-
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('coffee/', views.coffee_list, name='coffee_list'),
-    #path('cart/', views.cart_view, name='cart_view'),
+    path('cart/', views.cart_view, name='cart'),
     path('login/', views.login_view, name='login'),
     path('initiate-payment/', views.initiate_payment_view, name='initiate_payment'),
     path('payment-success/', views.payment_success, name='payment_success'),
     path('payment-failed/', views.payment_failed, name='payment_failed'),
     path('verify-payment/', views.verify_payment, name='verify_payment'),
-    
-    
-
-
+    path('start-payment/<int:coffee_id>/', views.start_payment, name='start_payment'),
 ]
