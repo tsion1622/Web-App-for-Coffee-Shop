46a6cb7502f0016b878ed29cf784f6536cb0ad29
Tsion Tamirat <tayu.16.enye@gmail.com>
2025-05-31 21:55:46 +0300

    updated the path

--- coffee/urls.py
+++ coffee/urls.py
@@ -4,11 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('coffee/', views.coffee_list, name='coffee_list'),
-    path('cart/', views.cart_view, name='cart'),
+     path('cart/', views.cart, name='cart'),  # <-- Make sure this exists
     path('login/', views.login_view, name='login'),
-    path('initiate-payment/', views.initiate_payment_view, name='initiate_payment'),
+    path('start-payment/<int:coffee_id>/', views.start_payment, name='start_payment'),
     path('payment-success/', views.payment_success, name='payment_success'),
     path('payment-failed/', views.payment_failed, name='payment_failed'),
-    path('verify-payment/', views.verify_payment, name='verify_payment'),
-    path('start-payment/<int:coffee_id>/', views.start_payment, name='start_payment'),
+    path('initiate-payment/', views.initiate_payment_view, name='initiate_payment'),
 ]
